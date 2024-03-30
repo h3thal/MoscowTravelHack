155a1ba7ecd338d7d8826c5d4130255686fa2265
Alexey Zinoviev <azinoviev@pnb.agency>
2024-03-30 19:51:24 +0300

    add Header component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./components/Header.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={'/tour'}>
+      <Header/>
+      <Routes>
+        <Route path={'/'} element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
